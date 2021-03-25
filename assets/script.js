8cb51e52af1380571db65028f327a26a0497f4d1
Jonathan Agatston <jagatston123@gmail.com>
2021-03-24 22:06:35 -0400

    added more code and comments

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
-
+// timer
+var timeleft = 50;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Time is up!";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+// questions for the coding quiz challenge
 var myQuestions = [
   {
     question: "Who invented javascript?",
     answers: {
       a: 'Yan Zhu',
       b: 'Brendan Eich',
-      c: 'Douglas Crockford'
+      c: 'Douglas Crockford',
+      d: 'none of the above'
     },
     correctAnswer: 'b'
   },
@@ -14,12 +26,43 @@ var myQuestions = [
     answers: {
       a: 'class',
       b: 'ID',
-      c: 'neither'
+      c: 'neither',
+      d: 'both'
     },
     correctAnswer: 'a'
-  }
+  },
+  {
+    question: "What keyword declares a variable?",
+    answers: {
+      a: 'var',
+      b: 'variable',
+      c: 'able',
+      d: 'none of the above'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "What surrounds a string?",
+    answers: {
+      a: 'brackets',
+      b: 'quotes',
+      c: 'curvy brackets',
+      d: 'none of the above'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "What values do booleans have?",
+    answers: {
+      a: 'true',
+      b: 'false',
+      c: 'neither',
+      d: 'both true and false'
+    },
+    correctAnswer: 'd'
+  },
 ];
-
+// calling back the variables
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
@@ -27,22 +70,13 @@ var submitButton = document.getElementById('submit');
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-  function showQuestions(questions, quizContainer){
-    // we'll need a place to store the output and the answer choices
+function showQuestions(questions, quizContainer){
     var output = [];
     var answers;
-
-    // for each question...
+    // for statement to generate quiz
     for(var i=0; i<questions.length; i++){
-      
-      // first reset the list of answers
       answers = [];
-
-      // for each available answer...
       for(letter in questions[i].answers){
-
-        // ...add an html radio button
         answers.push(
           '<label>'
             + '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -51,57 +85,40 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
           + '</label>'
         );
       }
-
-      // add this question and its answers to the output
       output.push(
         '<div class="question">' + questions[i].question + '</div>'
         + '<div class="answers">' + answers.join('') + '</div>'
       );
     }
-
-    // finally combine our output list into one string of html and put it on the page
     quizContainer.innerHTML = output.join('');
   }
 
-
-  function showResults(questions, quizContainer, resultsContainer){
-    
-    // gather answer containers from our quiz
+  function showResults(questions, quizContainer, resultsContainer){   
+    // variables to gather information
     var answerContainers = quizContainer.querySelectorAll('.answers');
-    
-    // keep track of user's answers
     var userAnswer = '';
     var numCorrect = 0;
     
-    // for each question...
+    // for else statement to return right or wrong answer
     for(var i=0; i<questions.length; i++){
-
-      // find selected answer
       userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-      
-      // if answer is correct
       if(userAnswer===questions[i].correctAnswer){
-        // add to the number of correct answers
         numCorrect++;
-        
-        // color the answers green
+        // colors the answers green or red depending on if they were right or not
         answerContainers[i].style.color = 'lightgreen';
       }
-      // if answer is wrong or blank
       else{
-        // color the answers red
         answerContainers[i].style.color = 'red';
       }
     }
-
-    // show number of correct answers out of total
+  // show how many questions were right
     resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
   }
 
   // show questions right away
   showQuestions(questions, quizContainer);
   
-  // on submit, show results
+  // submit button
   submitButton.onclick = function(){
     showResults(questions, quizContainer, resultsContainer);
   }
